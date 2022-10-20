1e87855b8888d20ffebe529ce88c2d0801687c02
Houssam Khalifeh <79322810+houssamkhalifeh@users.noreply.github.com>
2022-10-20 07:14:07 -0400

    Update Assembly_path_injection.cs

--- Pages/Model/Assembly_path_injection.cs
+++ Pages/Model/Assembly_path_injection.cs
@@ -6,9 +6,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 public class AssemblyPathInjectionHandler : PageModel  {
   public void ProcessRequest() {
-    string assemblyPath = Request.Query["assemblyPath"];
+    string assemblyPath1 = Request.Query["assemblyPath"];
     // BAD: Load assembly based on user input
-    var badAssembly = Assembly.LoadFile(assemblyPath);
+    var badAssembly = Assembly.LoadFile(assemblyPath1);
 
     // Method called on loaded assembly. If the user can control the loaded assembly, then this
     // could result in a remote code execution vulnerability
